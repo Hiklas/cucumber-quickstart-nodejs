1bb38c2a90768908e0d7ba3894efbfb492bdb816
Fiona Bianchi <fiona@hiklas.co.uk>
2016-02-10 16:44:02 +0000

    Just commiting some minor tweaks

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    mochaTest: {
+        test: {
+          options: {
+            reporter: 'spec',
+            captureFile: 'results.txt', // Optionally capture the reporter output to a file 
+            quiet: false, // Optionally suppress output to standard out (defaults to false) 
+            clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false) 
+          },
+          src: ['test/**/*.js']
+        }
+      }
+  });
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['mochaTest']);
+
+};
